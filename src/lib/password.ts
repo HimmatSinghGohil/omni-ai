2ed4d0e05f3a0130a9ed4c700b95443288da1f24
Himmat Singh Gohil <162715157+HimmatSinghGohil@users.noreply.github.com>
2026-08-15 23:38:45 +0530

    fix: use compatible bcryptjs import

--- src/lib/password.ts
+++ src/lib/password.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 
 export const hashPassword = (password: string) => bcrypt.hash(password, 12);
 export const verifyPassword = (password: string, hash: string) => bcrypt.compare(password, hash);
